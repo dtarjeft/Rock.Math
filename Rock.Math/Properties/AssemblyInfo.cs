69e0623748895ef69daf8b2514b399219431f986
MI\dtarjeft <DanTarjeft@QuickenLoans.com>
2016-10-17 15:56:44 -0400

    Housekeeping stuff

--- Rock.Math/Properties/AssemblyInfo.cs
+++ Rock.Math/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Rock.Math")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Some common math constructs.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Quicken Loans")]
+[assembly: AssemblyCompany("IT Team Einstein")]
 [assembly: AssemblyProduct("Rock.Math")]
 [assembly: AssemblyCopyright("Copyright © Quicken Loans 2014")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
